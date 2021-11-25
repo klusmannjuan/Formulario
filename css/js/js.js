543f4e0c82173c565752f4f8042b2347c2d67d68
klusmann <klusmannjuan@gmail.com>
2021-11-25 08:22:39 -0500

    KLUSMANN

---
+++ css/js/js.js
@@ -0,0 +1,11 @@
+const nombre = document.getElementById("name")
+const apellido  = document.getElementById("apellido")
+const email = document.getElementById("email")
+const document = document.getElementById("document")
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    if(nombre.value.length <6){
+        alert("Nombre muy corto")
+     
+    }
+}}
